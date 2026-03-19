efcfcffe0aa477ae6dc6552a3cdccd1303e6758e
Phan Dang Huy <huydtcute514@gmail.com>
2026-03-19 10:34:49 +0700

    Git

---
+++ src/main/java/com/example/PhanDangHuy/PhanDangHuyApplication.java
@@ -0,0 +1,13 @@
+package com.example.PhanDangHuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhanDangHuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhanDangHuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PhanDangHuy/PhanDangHuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PhanDangHuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhanDangHuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
