c20bf699067e9a672117ff18b3a74852980ab913
Phan Dang Huy <huydtcute514@gmail.com>
2026-03-26 13:05:32 +0700

    update view

--- src/main/java/com/example/PhanDangHuy/controllers/InfoController.java
+++ src/main/java/com/example/PhanDangHuy/controllers/InfoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class InfoController {
 
-    @GetMapping("/info")
+    @GetMapping({"/", "/info"})
     public String getInfo() {
         return "info";
     }
